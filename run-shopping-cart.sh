ca96e4a855055ca84a511b5381613f489578de2d
saikumarsagar <102918746+saikumarsagar@users.noreply.github.com>
2024-12-21 19:20:34 +0530

    Update run-shopping-cart.sh

--- run-shopping-cart.sh
+++ run-shopping-cart.sh
@@ -4,4 +4,4 @@
 #cd /home/ubuntu/Ekart/target
 
 # Run the Java application in the foreground and redirect stdout and stderr to a log file
-nohup java -jar shopping-cart-0.0.1-SNAPSHOT.jar >> /home/ubuntu/Ekart/shopping-cart.log 2>&1 &
+nohup java -jar target/shopping-cart-0.0.1-SNAPSHOT.jar >> /home/ubuntu/Ekart/shopping-cart.log 2>&1 &
